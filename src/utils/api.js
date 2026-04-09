7f27984c0afe68c352d36fb06fad1658eb0a2078
Badou02 <sembenediengalioune@gmail.com>
2026-04-09 13:34:45 +0000

    changement emailjs

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import emailjs from '@emailjs/browser';
 
-const SERVICE_ID = 'service_Vqmsbxt';
+const SERVICE_ID = 'service_vqnsbxt';
 const TEMPLATE_INSCRIPTION = 'template_0h6ehft';
 const TEMPLATE_CONTACT = 'template_bg9xf3a';
 const PUBLIC_KEY = 's5tVMe0TOzUH_ZOyA';
